dd5932dd6fb14e78e0de9b4e9416e16e2f0b13d4
Akiyasu-Hira <19s0251017@gmail.com>
2020-12-16 12:06:43 +0900

    URLの変更

--- public/js/button83.js
+++ public/js/button83.js
@@ -1,7 +1,6 @@
 // jQueryのボタン押下の処理
 $(function() {
   $('.button83').click(function(e) {
-    alert('牡丹八三ノ変更平');
-    window.location.href = 'https://www.yahoo.co.jp';
+    window.open("https://twitter.com/qst_exe", "twitter");
   });
 });
